394dec08cce5e42b8b6827bc01c62fcaac2398be
amir kourepaz <amir.kourepaz2001@gmail.com>
2025-02-18 19:17:12 +0330

    add new code for home page

--- src/app/pages/home/index.jsx
+++ src/app/pages/home/index.jsx
@@ -22,14 +22,18 @@ export default function Home() {
         </div>
         </div>
         <div className="main-prson">
-            <div className="men">
-
-            </div>
             <div className="women">
+            <h3 className="text-women"> پـوشـاک  زنـانـه <img src="./assets/images/peikan.svg" alt="" /></h3>
+            </div>
+            <div className="men">
+                <h3 className="text-men"> پـوشـاک مـردانـه  <img src="./assets/images/peikan.svg" alt="" /></h3>
 
             </div>
             <div className="position-1"></div>
             <div className="position-2"></div>
+            <img className="position-menprson" src="./assets/images/man.png" alt="" />
+            <img className="position-womenprson" src="./assets/images/women.png" alt="" />
+            <img className="position-women" src="./assets/images/women.svg" alt="" />
             <img className="position-men" src="./assets/images/men.svg" alt="" />
         </div>
         </>
--- src/app/ui/layouts/navbar.jsx
+++ src/app/ui/layouts/navbar.jsx
@@ -18,26 +18,97 @@ export default function navbar() {
     {
       id: 2,
       title: "وبلاگ",
-      url: "/blog",
+      url: "#",
+    },
+  ];
+
+  const Links = [
+    {
+      id: 0,
+      title: "کاپشن",
+      url: "#",
+    },
+    {
+      id: 1,
+      title: "ژاکت",
+      url: "#",
+    },
+    {
+      id: 2,
+      title: "کت و شلوار",
+      url: "#",
+    },
+    {
+      id: 4,
+      title: "شلوار",
+      url: "#",
+    },
+    {
+      id: 5,
+      title: "تیشرت",
+      url: "#",
+    },
+    {
+      id: 5,
+      title: "پیراهن",
+      url: "#",
     },
   ];
 
   return (
-    <nav>
-      <ul>
-        {links.map((link) => (
-          <li key={link.id}>
-            <Link to={link.url}> {link.title} </Link>
-          </li>
-        ))}
-      </ul>
+    <>
+      <nav>
+        <ul>
+          {links.map((link) => (
+            <li key={link.id}>
+              <Link to={link.url}> {link.title} </Link>
+            </li>
+          ))}
+        </ul>
 
-      <button className="cart">
-        سبد خرید
-        <div className="indicator">2</div>
-      </button>
-      <button className="profile-me">پروفایل من</button>
-      
-    </nav>
+        <button className="cart">
+          سبد خرید
+          <div className="indicator">2</div>
+        </button>
+        <button className="profile-me">پروفایل من</button>
+      </nav>
+      <div className="nav2">
+        <div className="input">
+
+        <svg className="svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M11.5 21C16.7467 21 21 16.7467 21 11.5C21 6.25329 16.7467 2 11.5 2C6.25329 2 2 6.25329 2 11.5C2 16.7467 6.25329 21 11.5 21Z"
+            stroke="#808080"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+          <path
+            d="M22 22L20 20"
+            stroke="#808080"
+            stroke-width="1.5"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          />
+        </svg>
+
+        <input type="search" placeholder="جستجو" />
+        </div>
+        <ul>
+          {Links.map((links) => (
+            <li key={links.id}>
+              <Link to={links.url}> {links.title} </Link>
+            </li>
+          ))}
+        </ul>
+        <h3>BLACK DARK</h3>
+      </div>
+    </>
   );
 }
